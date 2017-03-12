46dca5855fb194c82551c2f40a09a2b31b3f6005
Chanita Nuansri <nuansric@Chanitas-MacBook-Pro.local>
2017-03-11 23:37:22 -0600

    complete

---
+++ server.js
@@ -0,0 +1,55 @@
+// Dependencies
+var express = require("express");
+var bodyParser = require("body-parser");
+var logger = require("morgan");
+var mongoose = require("mongoose");
+
+// Our scraping tools
+var request = require("request");
+var cheerio = require("cheerio");
+var methodOverride = require("method-override");
+var exphbs = require("express-handlebars");
+
+// Set mongoose to leverage built in JavaScript ES6 Promises
+mongoose.Promise = Promise;
+var PORT = process.env.PORT || 8080;
+
+
+// Initialize Express
+var app = express();
+
+// Use morgan and body parser with our app
+app.use(logger("dev"));
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
+// Make public a static dir
+app.use(express.static("public"));
+app.use(methodOverride("_method"));
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+
+// Database configuration with mongoose
+mongoose.connect("mongodb://localhost/mongooseScrape");
+var db = mongoose.connection;
+
+// Show any mongoose errors
+db.on("error", function(error) {
+    console.log("Mongoose Error: ", error);
+});
+
+// Once logged in to the db through mongoose, log a success message
+db.once("open", function() {
+    console.log("Mongoose connection successful.");
+});
+
+require("./routes/routes.js")(app);
+
+
+// Listen on port 3000
+app.listen(PORT, function() {
+    console.log("App running on port 3000!");
+});
